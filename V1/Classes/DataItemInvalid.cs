91c3e1af7c58cf3a5d5f77126498a9c173f6450c
Karen Payne <Karen.1.Payne@oregon.gov>
2021-08-24 15:45:32 -0700

    Add project files.

---
+++ V1/Classes/DataItemInvalid.cs
@@ -0,0 +1,10 @@
+﻿namespace V1.Classes
+{
+    public class DataItemInvalid
+    {
+        public int Row { get; set; }
+        public string Line { get; set; }
+        public override string ToString() => $"[{Row}] '{Line}'";
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Exceptions and debugging
+
+Written with .NET Framework 4.8, zero advantages for this code sample to use .NET Core Framework as this is all about debugging.
+
+![#img](assets/ExceptionsDebugging.png)
+
+**TODO**
